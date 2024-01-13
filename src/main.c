24bf097510a8b87e5ab0d1950805b17c9de3f0f4
John William Vicente <johnwill.v.2017@gmail.com>
2024-01-13 08:41:18 -0300

    clear code

--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 #include<pthread.h>
 #include<stdlib.h>
 
+#define Matriz int**
+#define scan_int(x) scanf("%d", &x); 
+#define alloc(X,T,R) (R) malloc(X * sizeof (T*));
+
 void init();
 void fillMatrix();
 void freeVariables();
@@ -9,11 +13,11 @@ void freeVariables();
 typedef struct matriz_settings{
     int largura;
     int altura;
-} matriz_settings;
+} Matriz_settings;
 
 //COISAS DA MATRIZ PRINCIPAL
-matriz_settings m_settings;
-int** matriz;
+Matriz_settings m_settings;
+Matriz matriz;
 
 int main(){
 
@@ -25,17 +29,17 @@ int main(){
 
 void init(){
     printf("Insira a altura da matriz: ");
-    scanf("%d", &m_settings.altura);
+    scan_int(m_settings.altura);
 
     printf("Insira a largura da matriz: ");
-    scanf("%d", &m_settings.largura);
+    scan_int(m_settings.largura);
 
     printf("\nMATRIZ %02dX%02d\n",m_settings.altura, m_settings.largura );
 
-    matriz = (int**) malloc(m_settings.altura * sizeof (int*));
+    matriz = alloc(m_settings.altura, int, Matriz);
 
     for(int i=0;i<m_settings.altura;i++){
-         matriz[i] = (int*) malloc(m_settings.largura * sizeof (int*));
+         matriz[i] = alloc(m_settings.largura, int, int*);
     }
 
     fillMatrix();
